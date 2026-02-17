da5a5e5de62addaf5d860a3a1c2b1b7e6690d012
Tommaso Moro <tommasodoppiazetamoro@gmail.com>
2026-02-17 22:33:04 +0100

    Licenza

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QProcessEnvironment>
 #include <QSurfaceFormat>
 #include "mainwidget.h"
 #include "global.h"
@@ -7,13 +8,30 @@
   inizializza le informazioni in GLOBAL e genera la finestra
 */
 
-int main(int argc, char *argv[]) {
+namespace {
+void configureOpenGLFormat() {
     QSurfaceFormat format;
     format.setRenderableType(QSurfaceFormat::OpenGL);
-    format.setVersion(2, 1);
-    format.setProfile(QSurfaceFormat::CompatibilityProfile);
     format.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
+
+    // Il renderer di QuSoNEP usa OpenGL fixed-pipeline (glMatrixMode, glOrtho, ...).
+    // Con driver/Qt moderni può essere creato un Core Profile che disabilita queste API.
+    // Per questo di default chiediamo Compatibility Profile.
+    const bool modernMode = QProcessEnvironment::systemEnvironment().value(QString("QUSONEP_OPENGL_MODERN")) == QString("1");
+    if(modernMode) {
+        format.setVersion(3, 3);
+        format.setProfile(QSurfaceFormat::CoreProfile);
+    } else {
+        format.setVersion(2, 1);
+        format.setProfile(QSurfaceFormat::CompatibilityProfile);
+    }
+
     QSurfaceFormat::setDefaultFormat(format);
+}
+}
+
+int main(int argc, char *argv[]) {
+    configureOpenGLFormat();
 
     QApplication a(argc, argv);
     GLOBAL::riempiCombinazioni();
